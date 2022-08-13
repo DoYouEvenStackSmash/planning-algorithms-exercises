00dd5524f320a6dcd0213b0fa25c0b78d42c3e18
Jake <manumoomaw@gmail.com>
2022-08-12 20:57:56 -0700

    moved more functionality around

--- src/ch4/c_space_planning_project/primitive_sketch.py
+++ src/ch4/c_space_planning_project/primitive_sketch.py
@@ -31,42 +31,8 @@ from polygon_support import *
 from render_support import *
 
 
-colors = {
-  "black" : (0,0,0),
-  "yellow" : (255,255,0),
-  "cyan" : (0,255,255),
-  "green" : (0,255,0),
-  "magenta" : (255, 0, 255),
-  "red" : (255, 0, 0),
-  "white" : (255,255,255),
-  "indigo" : (48,79,254)
-}
-
 def create_display(width, height):
   return pygame.display.set_mode((width, height))
-
-# def draw_polygon(screen, point_set, color = (0,0,0)):
-#   pygame.draw.polygon(screen, color, point_set, width = 1)
-#   pygame.display.update()
-
-# def draw_dot(screen, point, color = (0,0,0)):
-#   pygame.draw.circle(screen, color, point, 4 , 4)
-#   pygame.display.update()
-
-# def draw_line(screen, point_set, color = (0,0,0)):
-#   s,e = point_set
-#   pygame.draw.aaline(screen, color, s, e)
-#   pygame.display.update()
-
-# def frame_draw_line(screen, point_set, color = (0,0,0)):
-#   s,e = point_set
-#   pygame.draw.aaline(screen, color, s, e)
-
-
-# def get_single_edge(o, pt1, pt2):
-#   l = create_edge(o,pt1,pt2)
-#   hp = HalfPlane(l)
-#   return hp
 
 
 def test_single_down_left_edge(w = 0, h = 0):
@@ -176,35 +142,6 @@ def test_right_triangle_polygon(w = 0, h = 0):
   p.half_planes_head = e1
   return p
 
-# def points_to_polygon(origin, point_list):
-#   o = Point(origin[0], origin[1])
-#   edge_list = []
-#   for i in range(1,len(point_list)):
-#     h = get_single_edge(o,point_list[i - 1], point_list[i])
-#     edge_list.append(Edge(h))
-#   h = get_single_edge(o,point_list[-1], point_list[0])
-#   edge_list.append(Edge(h))
-#   for i in range(1,len(edge_list)):
-#     edge_list[i - 1].m_next = edge_list[i]
-#   edge_list[-1].m_next = edge_list[0]
-#   p = Polygon()
-#   p.half_planes_head = edge_list[0]
-#   return p
-
-def display_in_vectors(screen, polygon = None):
-  x = polygon.get_in_vec_segments()
-  for i in x:
-    draw_line(screen, i, colors["yellow"])
-
-def display_out_vectors(screen, polygon = None):
-  x = polygon.get_out_vec_segments()
-  for i in x:
-    draw_line(screen, i, colors["red"])
-
-def display_polygon_edges(screen, polygon = None, color = colors["white"]):
-  x = polygon.get_segments()
-  for i in x:
-    draw_line(screen, i, color)
 
 def polygon_pygame_loop(screen, polygon = None):
   # print(polygon.get_segments())
@@ -253,11 +190,6 @@ def pygame_loop(screen,hp = None):
           print(f"{p} is unknown?")
         # print(hp.test_point(p))
         print(p)
-
-def display_polygon_attr(screen, polygon = None,color = colors["white"]):
-  display_polygon_edges(screen, polygon, color)
-  display_out_vectors(screen, polygon)
-  display_in_vectors(screen, polygon)
 
 
 def conv_func(theta):
--- src/ch4/c_space_planning_project/render_support.py
+++ src/ch4/c_space_planning_project/render_support.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 
 import pygame
+from polygon_support import *
 
+colors = {
+  "black" : (0,0,0),
+  "yellow" : (255,255,0),
+  "cyan" : (0,255,255),
+  "green" : (0,255,0),
+  "magenta" : (255, 0, 255),
+  "red" : (255, 0, 0),
+  "white" : (255,255,255),
+  "indigo" : (48,79,254)
+}
 # class DrawingFunctions:
 def draw_polygon(screen, point_set, color = (0,0,0)):
   pygame.draw.polygon(screen, color, point_set, width = 1)
@@ -19,3 +30,23 @@ def draw_line(screen, point_set, color = (0,0,0)):
 def frame_draw_line(screen, point_set, color = (0,0,0)):
   s,e = point_set
   pygame.draw.aaline(screen, color, s, e)
+
+def display_polygon_attr(screen, polygon = None,color = colors["white"]):
+  display_polygon_edges(screen, polygon, color)
+  display_out_vectors(screen, polygon)
+  display_in_vectors(screen, polygon)
+
+def display_in_vectors(screen, polygon = None):
+  x = polygon.get_in_vec_segments()
+  for i in x:
+    draw_line(screen, i, colors["yellow"])
+
+def display_out_vectors(screen, polygon = None):
+  x = polygon.get_out_vec_segments()
+  for i in x:
+    draw_line(screen, i, colors["red"])
+
+def display_polygon_edges(screen, polygon = None, color = colors["white"]):
+  x = polygon.get_segments()
+  for i in x:
+    draw_line(screen, i, color)
