587a72635c31a5107b393ff7a84aed8ddaf10258
Jake <manumoomaw@gmail.com>
2022-08-21 15:21:06 -0700

    init py and ready for new branch

--- src/ch4/manifold_project/double_torus_manifold.py
+++ src/ch4/manifold_project/double_torus_manifold.py
@@ -92,7 +92,7 @@ def double_torus(o, angle_degrees):
   ox,oy = o.get_x_min(), o.get_y_min()
   r = angle_degrees * np.pi / 180
   counter = 0
-  while not o.end_flag and counter < 2:
+  while not o.end_flag and counter < 10:
     lines.append(o.next_segment(o, ox, oy, r))
     ox, oy = lines[-1][1]
     counter+=1
