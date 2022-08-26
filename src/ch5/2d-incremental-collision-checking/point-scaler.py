3e8ec0ab72115be26021a1221ffd76e66cc106cf
Jake <manumoomaw@gmail.com>
2022-08-26 04:14:06 -0700

    we are finished with the feature detection

--- src/ch5/2d-incremental-collision-checking/point-scaler.py
+++ src/ch5/2d-incremental-collision-checking/point-scaler.py
@@ -76,7 +76,7 @@ def main():
   pl = build_point_list(i)
   # pl = [i for i in reversed(pl)]
   pl = scale(pl, .8)
-  # pl = move_point_list(pl, 0, 100)
+  # pl = move_point_list(pl, 200, 40)
   json_serializer(pl, get_filename(i))
   # pl = scale
   print(pl)
