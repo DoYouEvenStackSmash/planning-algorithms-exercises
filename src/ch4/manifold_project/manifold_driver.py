805c6fca419010570e8523fbe8125fc67fa3175a
Jake <manumoomaw@gmail.com>
2022-08-30 05:03:51 -0700

    added documentation, more usage

--- src/ch4/manifold_project/manifold_driver.py
+++ src/ch4/manifold_project/manifold_driver.py
@@ -14,6 +14,7 @@ import numpy as np
 import pygame
 import time
 
+''' drawing colors '''
 colors = {
   "black" : (0,0,0),
   "yellow" : (255,255,0),
@@ -25,6 +26,9 @@ colors = {
   "indigo" : (48,79,254)
 }
 
+''' 
+  helpers for rendering 
+'''
 def create_display(width, height):
   return pygame.display.set_mode((width, height))
 
@@ -42,15 +46,15 @@ def frame_draw_dot(screen, point, color = (0,0,0)):
 def clear_frame(screen):
   pygame.Surface.fill(screen, (0,0,0))
 
-
-# "-RS1": lambda o, deg: flat_cylinder(o, deg),
-# "-Ms": lambda o, deg: mobius_strip(o, deg),
-# "-T2": lambda o, deg: torus(o, deg),
-# "-Kb" : lambda o, deg: klein_bottle(o, deg),
-# "-RP2": lambda o, deg: projective_plane(o, deg),
-# "-S2" : lambda o, deg: two_sphere(o, deg),
-# "-DT2": lambda o, deg: double_torus(o, deg)
-
+fxn_help = {
+  "-RS1": "flat_cylinder",
+  "-Ms":  "mobius_strip",
+  "-T2":  "torus",
+  "-Kb" : "klein_bottle",
+  "-RP2": "projective_plane",
+  "-S2" : "two_sphere",
+  "-DT2": "double_torus"
+  }
 
 fxn_table = {
 "-RS1": lambda o, deg: flat_cylinder(o, deg),
@@ -61,20 +65,35 @@ fxn_table = {
 "-S2" : lambda o, deg: two_sphere(o, deg),
 "-DT2": lambda o, deg: double_torus(o, deg)
 }
+
+def print_args_help():
+  for i in set(fxn_table):
+    print(f"\t{i}\t{fxn_help[i]}")
+
+# helper for reading options from cli
 def gen_func():
   if len(sys.argv) < 2:
-    print("please enter option")
-    for i in set(fxn_table):
-      print(i)
+    print(f"USAGE:\n\t./manifold_driver.py [ARG]")
+    print_args_help()
     sys.exit()
   val = sys.argv[1]
   if val not in fxn_table:
-    print(f"{val} not recognized. please choose one of the following:")
-    for i in set(fxn_table):
-      print(i)
-      sys.exit()
+    print(f"{val} not recognized")
+    print(f"USAGE:\n\t./manifold_driver.py [ARG]")
+    print_args_help()
+    sys.exit()
   return fxn_table[val]
 
+'''
+  change_angle:
+    Sweeps an "infinite" line, constrained to origin point, from [0,90] degrees across a specified 2d manifold. 
+    Color Codes;
+      Bold Yellow is original line.
+      Green dot is start of line segment
+      Red Dot is end of line segment
+      Cyan is x axis
+      Magenta is y axis
+'''
 def change_angle(screen, o):
   bx = o.get_x_borders()
   by = o.get_y_borders()
@@ -82,24 +101,19 @@ def change_angle(screen, o):
   s = 1
   e = 90
   step = 1
+  # 1->90, 90->1
   for flip in range(2):
     for deg in range(s,e,step):
       clear_frame(screen)
+      # draw axes
       for i in range(2):
         frame_draw_line(screen, [bx[2 * i], bx[2 * i + 1]], colors["cyan"])
         frame_draw_line(screen, [by[2 * i], by[2 * i + 1]], colors["magenta"])
       
-      # l = flat_cylinder(o, deg)
+      # generate manifold segments for deg
       l = gen_fxn(o, deg)
-      # l = mobius_strip(o, deg)
-      # l = torus(o, deg)
-      # l = klein_bottle(o, deg)
-      # l = projective_plane(o, deg)
-      # l = two_sphere(o, deg)
-      # l = double_torus(o, deg)
       print(f"theta: {deg}")
-      # print(l)
-      # frame_draw_line(screen, [(o.get_x_min(), o.get_y_min()),(o.get_x_max(), o.get_y_max())], colors["red"])
+      
       frame_draw_bold_line(screen, l[0], colors["yellow"])
       frame_draw_dot(screen, l[0][0],colors["green"])
       frame_draw_dot(screen, l[0][1],colors["red"])
@@ -113,6 +127,10 @@ def change_angle(screen, o):
     step = step * -1
     s,e = e-1,s-1
 
+'''
+  single_angle:
+    Infinite line, constrained to origin point, on a specified 2d manifold for a hardcoded angle.
+  '''
 def single_angle(screen, o):
   bx = o.get_x_borders()
   by = o.get_y_borders()
