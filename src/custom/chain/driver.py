2a382f6c8ec986f3d10e25f2d072677056ecc9b7
Jake <manumoomaw@gmail.com>
2023-03-10 17:00:22 -0800

    added lerp render

--- src/custom/chain/driver.py
+++ src/custom/chain/driver.py
@@ -11,10 +11,30 @@ LSHIFT = 1
 LCTRL = 64
 SPACE = 32
 
+# render 
+def lerp_render(screen, n, spts, ev, l1_pts, l2_pts, pts):
+
+  # render lerp points
+  for sp in range(len(spts) - 1):
+    li = n * sp
+    ri = n * (sp + 1)
+    
+    for i in range(li,ri):
+      pafn.clear_frame(screen)
+      # render preexisting points, plus new point
+      for j in range(i):
+        pafn.frame_draw_dot(screen,pts[j],pafn.colors["cyan"])
+      
+      pafn.frame_draw_line(screen, (l1_pts[i], l2_pts[i]),pafn.colors["green"])
+      # pafn.frame_draw_line(screen, (origin, p),pafn.colors["tangerine"])
+      pafn.frame_draw_line(screen, (spts[sp],ev[sp]),pafn.colors["tangerine"])
+      pafn.frame_draw_line(screen, (ev[sp],spts[sp+1]),pafn.colors["tangerine"])
+      pygame.display.update()
+      time.sleep(0.005)
 
 def pygame_lerp_main(screen):
   k = 100
-  segment = 8
+  segment = 1
   origin = (k,k)
   while 1:
     for event in pygame.event.get():
@@ -29,7 +49,7 @@ def pygame_lerp_main(screen):
           continue
         p = pygame.mouse.get_pos()
 
-        if segment > 2:
+        if segment > 0:
           spts = []
           pts = []
           ev = []
@@ -37,133 +57,37 @@ def pygame_lerp_main(screen):
           l2_pts = []
 
           seg_step = 1 / segment
-
+          # break origin-target segment into regions
           for i in range(segment):
             spts.append(gfn.lerp(origin, p, i * seg_step))
           spts.append(p)
+          
+          # get equilateral vertex of each segment
           sign = 1
           for i in range(len(spts) - 1):
             ev.append(gfn.get_equilateral_vertex(spts[i], spts[i+1], sign))
             sign = sign * -1
-
+          
+          # calculate lerp points
           n = 100
           step = 1 / n
           for sp in range(len(spts) - 1):
             for i in range(n):
+              # lerp between origin and equilateral vertex
               l1 = gfn.lerp(spts[sp], ev[sp],step * i)
+              # lerp between equilateral vertex and target
               l2 = gfn.lerp(ev[sp], spts[sp + 1], step * i)
+              # lerp between lerps
               m1 = gfn.lerp(l1, l2, step * i)
 
               l1_pts.append(l1)
               l2_pts.append(l2)
               pts.append(m1)
-          for sp in range(len(spts) - 1):
-            li = n * sp
-            ri = n * (sp + 1)
-            interval = n * (sp + 1)
-            
-            for i in range(li,ri):
-              pafn.clear_frame(screen)
-              for j in range(i):
-                pafn.frame_draw_dot(screen,pts[j],pafn.colors["cyan"])
-              
-              pafn.frame_draw_line(screen, (l1_pts[i], l2_pts[i]),pafn.colors["green"])
-              # pafn.frame_draw_line(screen, (origin, p),pafn.colors["tangerine"])
-              pafn.frame_draw_line(screen, (spts[sp],ev[sp]),pafn.colors["tangerine"])
-              pafn.frame_draw_line(screen, (ev[sp],spts[sp+1]),pafn.colors["tangerine"])
-              pygame.display.update()
-              time.sleep(0.005)
+
+          lerp_render(screen, n, spts, ev, l1_pts, l2_pts, pts)
+
 
           continue
-
-        ev = gfn.get_equilateral_vertex(origin, p)
-        mp = gfn.get_midpoint(origin, p)
-        evs = []
-        evs.append(gfn.get_equilateral_vertex(origin, mp))
-        evs.append(gfn.get_equilateral_vertex(mp, p, -1))
-        if segment == 2:
-          n = 100
-          step = 1 / n
-          pts = []
-          l1_pts = []
-          l2_pts = []
-
-          for i in range(n):
-            l1 = gfn.lerp(origin, evs[0], step * i)
-            l2 = gfn.lerp(evs[0], mp, step * i)
-            m1 = gfn.lerp(l1, l2,step * i)
-            pts.append(m1)
-            l1_pts.append(l1)
-            l2_pts.append(l2)
-
-          for i in range(n):
-            l1 = gfn.lerp(mp, evs[1], step * i)
-            l2 = gfn.lerp(evs[1], p, step * i)
-            m1 = gfn.lerp(l1, l2,step * i)
-            pts.append(m1)
-            l1_pts.append(l1)
-            l2_pts.append(l2)
-          
-          for i in range(int(len(pts)/2)):
-            pafn.clear_frame(screen)
-            for j in range(i):
-              pafn.frame_draw_dot(screen,pts[j],pafn.colors["cyan"])
-            pafn.frame_draw_line(screen, (l1_pts[i], l2_pts[i]),pafn.colors["green"])
-            # pafn.frame_draw_line(screen, (origin, p),pafn.colors["tangerine"])
-            pafn.frame_draw_line(screen, (origin,evs[0]),pafn.colors["tangerine"])
-            pafn.frame_draw_line(screen, (mp,evs[0]),pafn.colors["tangerine"])
-            pygame.display.update()
-            time.sleep(0.01)
-          for i in range(int(len(pts)/2),len(pts)):
-            pafn.clear_frame(screen)
-            for j in range(i):
-              pafn.frame_draw_dot(screen,pts[j],pafn.colors["cyan"])
-            pafn.frame_draw_line(screen, (l1_pts[i], l2_pts[i]),pafn.colors["green"])
-            # pafn.frame_draw_line(screen, (origin, p),pafn.colors["tangerine"])
-            pafn.frame_draw_line(screen, (mp,evs[1]),pafn.colors["tangerine"])
-            pafn.frame_draw_line(screen, (p,evs[1]),pafn.colors["tangerine"])
-            pygame.display.update()
-            time.sleep(0.01)
-        
-        elif segment == 1:
-          n = 100
-          step = 1 / n
-          pts = []
-          l1_pts = []
-          l2_pts = []
-          
-          for i in range(n):
-
-            l1 = gfn.lerp(origin, ev, step * i)
-            l2 = gfn.lerp(ev, p, step * i)
-            m1 = gfn.lerp(l1, l2,step * i)
-            pts.append(m1)
-            l1_pts.append(l1)
-            l2_pts.append(l2)
-
-          for i in range(n):
-            pafn.clear_frame(screen)
-            for j in range(i):
-              pafn.frame_draw_dot(screen,pts[j],pafn.colors["cyan"])
-            pafn.frame_draw_line(screen, (l1_pts[i], l2_pts[i]),pafn.colors["green"])
-            # pafn.frame_draw_line(screen, (origin, p),pafn.colors["tangerine"])
-            pafn.frame_draw_line(screen, (origin,ev),pafn.colors["tangerine"])
-            pafn.frame_draw_line(screen, (p,ev),pafn.colors["tangerine"])
-            pygame.display.update()
-            time.sleep(0.01)
-        continue
-        l1 = gfn.get_midpoint(p,ev)
-        l2 = gfn.get_midpoint(origin, ev)
-        pafn.frame_draw_line(screen, (l1, l2),pafn.colors["tangerine"])
-        print(ev)
-        pafn.frame_draw_line(screen,(p,ev),pafn.colors["indigo"])
-        pafn.frame_draw_line(screen,(ev,origin),pafn.colors["indigo"])
-        pafn.frame_draw_dot(screen,ev,pafn.colors["cyan"])
-        pafn.frame_draw_line(screen,(origin,p),pafn.colors["indigo"])
-        pafn.frame_draw_dot(screen,p,pafn.colors["yellow"])
-        pafn.frame_draw_dot(screen,origin,pafn.colors["green"])
-
-        pygame.display.update()
 
     
 
