77877ea3856b7728ee9b10e71b212f2bb6b8ab7e
Jake <manumoomaw@gmail.com>
2023-07-10 22:57:00 -0700

    added rapidly exploring random tree

---
+++ src/ch5/random-dense-tree/render_support.py
@@ -0,0 +1,350 @@
+#!/usr/bin/python3
+import numpy as np
+import pygame
+import time
+
+
+class TransformFxns:
+    """
+    Transform functions
+    """
+
+    def rotation_matrix(rad_theta):
+        """
+        Creates a rotation matrix
+        Returns a 2x2 numpy array
+        """
+        return np.array(
+            [
+                [np.cos(rad_theta), -np.sin(rad_theta)],
+                [np.sin(rad_theta), np.cos(rad_theta)],
+            ]
+        )
+
+    def calculate_rotation_matrix(rad_theta, step_count=30):
+        """
+        Wrapper for rotation matrix calculation including step count
+        Returns a 2x2 numpy array
+        """
+        return TransformFxns.rotation_matrix(np.divide(rad_theta, step_count))
+
+    def calculate_rotation(origin, target, last_target):
+        """
+        Calculates rotation as a delta theta between last target and current target
+        returns an angle theta
+        """
+        if last_target == target:
+            return 0, target
+        rad, r = MathFxns.car2pol(origin, target)
+        rad2, r2 = MathFxns.car2pol(origin, last_target)
+        rotation = rad - rad2
+        # adjust to make sure rotation is in interval [-pi, pi]
+        if rotation > np.pi:
+            rotation = rotation - (2 * np.pi)
+        if rotation < -np.pi:
+            rotation = rotation + (2 * np.pi)
+
+        return rotation, target
+
+    def rotate_point_set(origin, point_set, rot_mat):
+        """
+        Rotates a set of points using rotation matrix
+        Wrapper for rotate point
+
+        Returns a list of points
+        """
+        nps = []
+        for i in point_set:
+            nps.append(TransformFxns.rotate_point(origin, i, rot_mat))
+        return nps
+
+    def rotate_point(origin, pt, rot_mat):
+        """
+        Rotates a single point about an origin based on the rotation matrix
+        Returns a point (x, y)
+        """
+        x_o, y_o = origin
+        lp_x, lp_y = pt
+
+        step = np.matmul(rot_mat, np.array([[lp_x - x_o], [lp_y - y_o]]))
+        return (step[0][0] + x_o, step[1][0] + y_o)
+
+    def get_translation_function(origin, t, steps=1):
+        """
+        Derives incremental displacement for a polygon to reach a target point
+        in some number of steps
+        Returns a tuple containing displacement for x,y, and a constant
+        """
+        r, theta = MathFxns.car2pol(origin, t)
+        d = MathFxns.euclidean_dist(origin, t)
+        sign = 1
+        if d < 0 and r < 0:
+            sign = -1
+        r = r * d * sign
+        # d = MathFxns.euclidean_dist(origin, t)
+        # print(r)
+        # return MathFxns.pol2car(, r, theta)
+        step_dist = r / steps
+        x_step = step_dist * np.cos(theta)
+        y_step = step_dist * np.sin(theta)
+        val = (x_step, y_step)
+        return val
+
+
+class MathFxns:
+    """
+    Math helper functions
+    """
+
+    def euclidean_dist(p1, p2):
+        """
+        Calculates euclidean distance between two points
+        Returns a scalar value
+        """
+        return np.sqrt(np.square(p1[0] - p2[0]) + np.square(p1[1] - p2[1]))
+
+    def car2pol(origin, pt):
+        """
+        Converts a pair of points into a vector
+        returns a vector (radians, radius)
+        """
+        x, y = pt
+        ox, oy = origin
+        rad = np.arctan2(y - oy, x - ox)
+        r = MathFxns.euclidean_dist(origin, pt)
+        return (rad, r)
+
+    def pol2car(pt, r, theta):
+        """
+        Convert polar coordinate to cartesian
+        Returns a point
+        """
+        ox, oy = pt
+        x = r * np.cos(theta)
+        y = r * np.sin(theta)
+        return (ox + x, oy + y)
+
+    def correct_angle(rad_theta):
+        """
+        Normalizes a negative angle theta, created by arctan2
+        Returns an angle between -pi/2 and 2pi
+        """
+        if rad_theta <= -np.pi / 2:
+            rad_theta = rad_theta + 2 * np.pi
+        return rad_theta
+
+
+class GeometryFxns:
+    """
+    Geometry helper functions
+    """
+
+    def get_equilateral_vertex(pt1, pt2, sign=1):
+        """
+        Calculates the vertex of an equilateral triangle
+        Returns a point
+        """
+        rad, r = MathFxns.car2pol(pt1, pt2)
+        nx = r * np.cos(rad + np.pi * sign / 3)
+        ny = r * np.sin(rad + np.pi * sign / 3)
+        return (nx + pt1[0], ny + pt1[1])
+
+    def get_isosceles_vertex(pt1, pt2, sign=1, theta=45):
+        rad, r = MathFxns.car2pol(pt1, pt2)
+        radians = theta * (np.pi / 180) * sign
+        nx = r * np.cos(rad + radians)
+        ny = r * np.sin(rad + radians)
+        return (nx + pt1[0], ny + pt1[1])
+
+    def get_midpoint(pt1, pt2):
+        """
+        Calculates the midpoint of the segment connecting two points
+        Returns a point
+        """
+        rad, r = MathFxns.car2pol(pt1, pt2)
+        nx = r / 2 * np.cos(rad)
+        ny = r / 2 * np.sin(rad)
+        return (nx + pt1[0], ny + pt1[1])
+
+    def lerp(pt1, pt2, t):
+        """
+        Lerp between two points
+        Returns a point
+        """
+        rad, r = MathFxns.car2pol(pt1, pt2)
+        nx = r * t * np.cos(rad)
+        ny = r * t * np.sin(rad)
+        return (nx + pt1[0], ny + pt1[1])
+
+    def lerp_list(p1, p2, n=100):
+        """
+        Lerp helper function for two points
+        Returns a list of points
+        """
+        pts = []
+        step = 1 / n
+        for i in range(n):
+            pts.append(GeometryFxns.lerp(p1, p2, step * i))
+        pts.append(p2)
+        return pts
+
+    def get_unit_norm_angle(ray_origin, ray_target, switch=False):
+        """
+        Returns the angle in radians of the vector normal to the line between two
+        points.
+        Switch is used by caller functions to flip the angle around the unit circle.
+        """
+        x1, y1 = ray_origin
+        x2, y2 = ray_target
+
+        rad_theta = np.arctan2(y2 - y1, x2 - x1)
+        # print(rad_theta)
+        rad_prime = rad_theta
+        if rad_prime < -np.pi / 2:
+            rad_prime = 2 * np.pi + rad_prime
+        rad_prime = rad_prime - np.pi / 2
+
+        if switch:
+            if rad_prime > 0:
+                return rad_prime - np.pi
+            return rad_prime + np.pi
+        return rad_prime
+
+    def cubic_lerp_calculate(pts, n=100):
+        """
+        Cubic lerp function for a list of at least 4 points
+        Returns linear interpolation between pairs of points
+          l1=(A,B)
+          l2=(B,C)
+          l3=(C,D)
+          m1 = (l1,l2)
+          m2 = (l2,l3)
+          p1 = (m1, m2)
+        """
+        l1 = GeometryFxns.lerp_list(pts[0], pts[1])
+        l2 = GeometryFxns.lerp_list(pts[1], pts[2])
+        l3 = GeometryFxns.lerp_list(pts[2], pts[3])
+        m1 = []
+        m2 = []
+        step = 1 / n
+
+        for i in range(n):
+            m1.append(GeometryFxns.lerp(l1[i], l2[i], i * step))
+            m2.append(GeometryFxns.lerp(l2[i], l3[i], i * step))
+        m1.append(GeometryFxns.lerp(l1[-1], l2[-1], 1))
+        m2.append(GeometryFxns.lerp(l2[-1], l3[-1], 1))
+
+        p1 = []
+        for i in range(n):
+            p1.append(GeometryFxns.lerp(m1[i], m2[i], i * step))
+        p1.append(GeometryFxns.lerp(m1[-1], m2[-1], 1))
+        return l1, l2, l3, m1, m2, p1
+
+
+class PygameArtFxns:
+    """set of colors"""
+
+    colors = {
+        "black": (0, 0, 0),
+        "indigo": (48, 79, 254),
+        "faded-blue": (15, 173, 237),
+        "sky-blue": (111, 205, 244),
+        "darker-green": (47, 178, 35),
+        "yellow": (255, 255, 0),
+        "tangerine": (255, 119, 34),
+        "pink": (255, 122, 173),
+        "cyan": (0, 255, 255),
+        "green": (0, 255, 0),
+        "magenta": (255, 0, 255),
+        "red": (255, 0, 0),
+        "white": (255, 255, 255),
+        "dimgray": (105, 105, 105),
+        "lightslategray": (119, 136, 153),
+        "silver": (192, 192, 192),
+        "forestgreen": (34, 139, 34),
+        "seagreen": (46, 139, 87),
+        "lawngreen": (124, 252, 0),
+    }
+
+    def create_display(width, height):
+        """
+        Initializes a pygame display
+        width, height := pixel dimensions of the display
+        Returns a pygame display object
+
+        """
+        return pygame.display.set_mode((width, height))
+
+    def frame_draw_polygon(screen, point_set, color=(0, 0, 0)):
+        """
+        Draws a polygon of specified color
+        Returns nothing
+        """
+        pygame.draw.polygon(screen, color, point_set, width=2)
+
+    def frame_draw_line(screen, point_set, color=(0, 0, 0)):
+        """
+        Draws a thin line given a pair of points (start, end)
+        Returns nothing
+        """
+        s, e = point_set
+        pygame.draw.aaline(screen, color, s, e)
+
+    def frame_draw_bold_line(screen, point_set, color=(0, 0, 0)):
+        """
+        Draws a bold line given a pair of points (start, end)
+        Returns nothing
+        """
+        s, e = point_set
+        pygame.draw.line(screen, color, s, e, width=4)
+
+    def frame_draw_dot(screen, point, color=(0, 0, 0), width=0, thickness=4):
+        """
+        Draws a single dot given a point (x, y)
+        Returns nothing
+        """
+        pygame.draw.circle(screen, color, point, thickness, width)
+
+    def clear_frame(screen, color=(255, 255, 255)):
+        """
+        Resets the pygame display to a given color
+        Returns nothing
+        """
+        pygame.Surface.fill(screen, PygameArtFxns.colors["silver"])
+
+    def draw_lines_between_points(screen, pts, color=(255, 255, 255)):
+        """
+        Given an ordered list of points, draws lines connecting each pair
+        Returns nothing
+        """
+        color_arr = [
+            PygameArtFxns.colors["red"],
+            PygameArtFxns.colors["yellow"],
+            PygameArtFxns.colors["white"],
+        ]
+        for i in range(1, len(pts)):
+            # frame_draw_dot(screen, pts[i - 1], PygameArtFxns.colors["red"])
+
+            PygameArtFxns.frame_draw_line(screen, (pts[i - 1], pts[i]), color)
+
+        # frame_draw_dot(screen, pts[-1], PygameArtFxns.colors["red"])
+        PygameArtFxns.frame_draw_line(screen, (pts[-1], pts[0]), color)
+
+    def frame_draw_filled_polygon(screen, pts, color=(255, 255, 255)):
+        """
+        Draws a filled polygon
+        Does not return
+        """
+        pygame.gfxdraw.filled_polygon(screen, pts, color)
+
+    def frame_draw_cross(screen, pt, color=(255, 255, 255), size=10):
+        """
+        Draws a small cross
+        Does not return
+        """
+        x, y = pt
+        sz = size
+        l1 = [(x + sz, y), (x - sz, y)]
+        l2 = [(x, y + sz), (x, y - sz)]
+        PygameArtFxns.frame_draw_bold_line(screen, l1, color)
+        PygameArtFxns.frame_draw_bold_line(screen, l2, color)
---
+++ src/ch5/random-dense-tree/triangle-driver.py
@@ -0,0 +1,373 @@
+#!/usr/bin/python3
+import pygame
+from render_support import GeometryFxns as gfn
+from render_support import MathFxns as mfn
+from render_support import TransformFxns as tfn
+from render_support import PygameArtFxns as pafn
+import time
+import sys
+import numpy as np
+
+WHITE = 0
+GRAY = 1
+BLACK = 2
+# def segment_driver(screen):
+class Vertex:
+  def __init__(self, coordinate):
+    self.pt = coordinate
+    self.neighbor_set = set()
+    self.visited = WHITE
+    self.neighbor_counter = 0
+  
+  def get_coord(self):
+    return self.pt
+  
+  # def add_neighbor(self, target_pt):
+  #   self.neighbor_list.append(target_pt)
+
+class Edge:
+    def __init__(self, pt1, pt2):
+        self.pt1 = pt1
+        self.pt2 = pt2
+
+    def get_triangle_theta(self, new_pt):
+        delta1 = abs(get_delta_theta(self.pt2, self.pt1, new_pt))
+        delta2 = abs(get_delta_theta(self.pt1, self.pt2, new_pt))
+        return delta1 + delta2
+
+    def get_out_of_normal(self, new_pt):
+        delta2 = abs(get_delta_theta(self.pt1, self.pt2, new_pt))
+        delta1 = abs(get_delta_theta(self.pt2, self.pt1, new_pt))
+        return delta1 > np.pi / 2 or delta2 > np.pi / 2
+
+    def get_triangle_perimeter(self, new_pt):
+        r1 = mfn.euclidean_dist(self.pt1, new_pt)
+        r2 = mfn.euclidean_dist(self.pt2, new_pt)
+
+    def get_normal_dist(self, new_pt):
+        h = mfn.euclidean_dist(self.pt1, new_pt)
+        theta = abs(get_delta_theta(self.pt2, self.pt1, new_pt))
+        return np.sin(theta) * h
+
+    def get_normal_pt(self, new_pt):
+        h = mfn.euclidean_dist(self.pt1, new_pt)
+        theta = abs(get_delta_theta(self.pt2, self.pt1, new_pt))
+        direction, r = mfn.car2pol(self.pt1, self.pt2)
+        d = h * np.cos(theta)
+        normal_pt = mfn.pol2car(self.pt1, d, direction)
+        return normal_pt
+
+    def break_edge(self, new_pt):
+        d1 = mfn.euclidean_dist(self.pt1, new_pt)
+        d2 = mfn.euclidean_dist(self.pt2, new_pt)
+        new_edge = None
+        if d1 < d2:
+            new_edge = Edge(self.pt1, new_pt)
+            self.pt1 = new_pt
+        else:
+            new_edge = Edge(new_pt, self.pt2)
+            self.pt2 = new_pt
+        # self.pt2 = new_pt
+        return new_edge
+
+    def get_pts(self):
+        return (self.pt1, self.pt2)
+
+
+def gen_van_der_corput(k_bits=4):
+    max_val = np.square(k_bits)
+    seq = []
+    for i in range(max_val):
+        k = bin(i)[2:][::-1]
+        print(k)
+        if len(k) < k_bits:
+            k = k + f"{(k_bits - len(k)) * '0'}"
+        n = 0
+        for j in range(len(k)):
+            n += int(k[-1 - j]) * np.power(2, j)
+        seq.append(np.divide(n, max_val))
+    return seq
+
+
+def adjust_angle(theta):
+    """
+    adjusts some theta to arctan2 interval [0,pi] and [-pi, 0]
+    """
+    if theta > np.pi:
+        theta = theta + -2 * np.pi
+    elif theta < -np.pi:
+        theta = theta + 2 * np.pi
+
+    return theta
+
+
+def get_delta_theta(endpoint_1, vertex, endpoint_2):
+    theta1, rad1 = mfn.car2pol(vertex, endpoint_1)
+    theta2, rad2 = mfn.car2pol(vertex, endpoint_2)
+
+    delta_theta = adjust_angle(theta2 - theta1)
+    return delta_theta
+
+
+def add_new_edge(edge_list, point_set, target_pt):
+    nearest_el = []
+    sortkey = lambda et: et[0]
+    # get distance to all valid edges
+    for i in range(len(edge_list)):
+        if not edge_list[i].get_out_of_normal(target_pt):
+            nearest_el.append((edge_list[i].get_normal_dist(target_pt), i))
+
+    # get distance to all points
+    nearest_pts = []
+    for pt in point_set:
+        nearest_pts.append((mfn.euclidean_dist(pt, target_pt), pt))
+    # nearest_pts.append((1e9, (-1, -1)))
+    nearest_pts = sorted(nearest_pts, key=sortkey)
+    # print(nearest_pts)
+    nearest_el = sorted(nearest_el, key=sortkey)
+    edge_dist = 1e9  # set edge dist to some big constant
+    pt_dist = 1e9
+    if not len(nearest_pts) and not len(nearest_el):
+        point_set.add(target_pt)
+        return
+
+    if len(nearest_el):
+        edge_dist = nearest_el[0][0]
+
+    pt_dist = nearest_pts[0][0]
+    new_edge = None
+    if pt_dist < edge_dist:
+        new_edge = Edge(nearest_pts[0][1], target_pt)
+    else:
+        norm_pt = edge_list[nearest_el[0][1]].get_normal_pt(target_pt)
+        print(norm_pt)
+        intermediate_edge = edge_list[nearest_el[0][1]].break_edge(norm_pt)
+        edge_list.append(intermediate_edge)
+        point_set.add(norm_pt)
+        new_edge = Edge(norm_pt, target_pt)
+
+    point_set.add(target_pt)
+    edge_list.append(new_edge)
+
+
+def get_nearest_pt(edge_list, point_set, target_pt):
+    nearest_el = []
+    sortkey = lambda et: et[0]
+    # get distance to all valid edges
+    for i in range(len(edge_list)):
+        if not edge_list[i].get_out_of_normal(target_pt):
+            nearest_el.append((edge_list[i].get_normal_dist(target_pt), i))
+
+    # get distance to all points
+    nearest_pts = []
+    for pt in point_set:
+        nearest_pts.append((mfn.euclidean_dist(pt, target_pt), pt))
+    # nearest_pts.append((1e9, (-1, -1)))
+    nearest_pts = sorted(nearest_pts, key=sortkey)
+    # print(nearest_pts)
+    nearest_el = sorted(nearest_el, key=sortkey)
+    edge_dist = 1e9  # set edge dist to some big constant
+    pt_dist = 1e9
+    # if not len(nearest_pts) and not len(nearest_el):
+    #   point_set.add(target_pt)
+    #   return
+
+    if len(nearest_el):
+        edge_dist = nearest_el[0][0]
+
+    pt_dist = nearest_pts[0][0]
+    new_edge = None
+    nearest_pt = None
+    if pt_dist < edge_dist:
+        nearest_pt = nearest_pts[0][1]
+    else:
+        nearest_pt = edge_list[nearest_el[0][1]].get_normal_pt(target_pt)
+
+    return nearest_pt
+
+
+def interactive_triangle_test(screen):
+    endpt_1 = (500, 500)
+    endpt_2 = (600, 600)
+    point_set = {endpt_1, endpt_2}
+    edge_list = [Edge(endpt_1, endpt_2)]
+    for e in edge_list:
+        pt1, pt2 = e.get_pts()
+        pafn.frame_draw_dot(screen, pt1, pafn.colors["white"])
+        pafn.frame_draw_dot(screen, pt2, pafn.colors["white"])
+        pafn.frame_draw_line(screen, (pt1, pt2), pafn.colors["indigo"])
+    pygame.display.update()
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                p = pygame.mouse.get_pos()
+                pafn.clear_frame(screen)
+
+                add_new_edge(edge_list, point_set, p)
+                for e in edge_list:
+                    pt1, pt2 = e.get_pts()
+                    pafn.frame_draw_dot(screen, pt1, pafn.colors["white"])
+                    pafn.frame_draw_dot(screen, pt2, pafn.colors["white"])
+                    pafn.frame_draw_line(screen, (pt1, pt2), pafn.colors["indigo"])
+                pygame.display.update()
+
+
+def generate_points(origin=(500, 500), dim=500, input_sequence=[]):
+    pl = []
+    x, y = origin
+    for i in input_sequence:
+        val = i - 0.5
+        x = dim * val + dim
+        y = dim * 2 - x
+        pl.append((x, x))
+    return pl
+
+
+def get_rand_sequence(k=32):
+    rng = np.random.default_rng(12345)
+    rand_val = lambda: (rng.uniform()) * 1000
+    pl = []
+    for i in range(k):
+        x = rand_val()
+        y = rand_val()
+        pl.append((x, y))
+    return pl
+
+
+def sequence_test(screen, input_points):
+    edge_list = []
+    point_set = set()
+    for p in input_points:
+        # pafn.frame_draw_dot(i)
+        pafn.clear_frame(screen)
+
+        add_new_edge(edge_list, point_set, p)
+        for e in edge_list:
+            pt1, pt2 = e.get_pts()
+            pafn.frame_draw_dot(screen, pt1, pafn.colors["white"])
+            pafn.frame_draw_dot(screen, pt2, pafn.colors["white"])
+            pafn.frame_draw_line(screen, (pt1, pt2), pafn.colors["indigo"])
+        pygame.display.update()
+        time.sleep(0.01)
+
+
+def get_farthest_pt(obstacle_list, point_list, distance_thres=30):
+    last_pt = None
+    for i in range(len(point_list)):
+        p = point_list[i]
+        for o in obstacle_list:
+            d = mfn.euclidean_dist(p, o)
+            if d < distance_thres:
+                print("breaking")
+                return last_pt
+        last_pt = p
+    return last_pt
+
+
+def sequence_with_goal_test(screen, input_points, goal_pt=(900, 100)):
+    # obs_list = [(400, 400), (700, 700), (800, 800), (900, 800), (800, 900)]
+    obs_list = get_rand_sequence(40)#generate_points(input_sequence=gen_van_der_corput(5))
+    # obs_list = []
+    obs_rad = 20
+
+    edge_list = []
+    initial_point = (100,900)
+    point_set = {initial_point}
+    step_size = obs_rad / 4
+    rng = np.random.default_rng(32345)
+    rand_val = lambda: (int(rng.uniform(0, 100)))
+    for i in range(len(input_points)):
+        p = input_points[i]
+        if rand_val() == 1:
+            p = goal_pt
+        nearest_pt = get_nearest_pt(edge_list, point_set, p)
+        step_count = 100#max(int(mfn.euclidean_dist(nearest_pt, p) / step_size), 10) * 2
+        point_list = gfn.lerp_list(nearest_pt, p, step_count)
+        
+        # print(point_list)
+
+        target_pt = get_farthest_pt(obs_list, point_list, 70)
+        if target_pt != None:
+            add_new_edge(edge_list, point_set, target_pt)
+        for e in edge_list:
+            pt1, pt2 = e.get_pts()
+            pafn.frame_draw_dot(screen, pt1, pafn.colors["white"])
+            pafn.frame_draw_dot(screen, pt2, pafn.colors["white"])
+            pafn.frame_draw_line(screen, (pt1, pt2), pafn.colors["indigo"])
+        for o_center in obs_list:
+            pafn.frame_draw_dot(screen, o_center, pafn.colors["red"], obs_rad, obs_rad)
+
+        pygame.display.update()
+        time.sleep(0.01)
+        if target_pt == goal_pt:
+            print("path found!")
+            break
+    
+    vertex_dict = {}
+    for pt in point_set:
+      vertex_dict[pt] = Vertex(pt)
+
+    for edge in edge_list:
+      p1,p2 = edge.get_pts()
+      vertex_dict[p1].neighbor_set.add(p2)
+      vertex_dict[p2].neighbor_set.add(p1)
+    
+    
+    for vertex in vertex_dict:
+      vertex_dict[vertex].neighbor_set = list(vertex_dict[vertex].neighbor_set)
+    # queue = []
+    stack = [] # stack.append(), stack.pop()
+    curr_vertex = vertex_dict[initial_point]
+    curr_vertex.visited = GRAY
+    stack.append(curr_vertex)
+    # stack.append(curr_vertex)
+    # while len(queue) != 0:
+    # for pt in curr_vertex.neighbor_set:
+    while stack[-1].get_coord() != target_pt:
+      # stack[-1].
+      
+      if stack[-1].neighbor_counter < len(stack[-1].neighbor_set):
+        stack[-1].neighbor_counter += 1
+        if vertex_dict[stack[-1].neighbor_set[stack[-1].neighbor_counter - 1]].visited == WHITE:
+          stack.append(vertex_dict[stack[-1].neighbor_set[stack[-1].neighbor_counter - 1]])
+          stack[-1].visited = GRAY
+      else:
+        stack[-1].visited = BLACK
+        stack.pop()
+      
+    print(stack)
+    for v in range(1, len(stack)):
+      pafn.frame_draw_cross(screen, stack[v - 1].get_coord(), pafn.colors["yellow"])
+      pafn.frame_draw_bold_line(screen, (stack[v - 1].get_coord(), stack[v].get_coord()), pafn.colors["green"])
+      pygame.display.update()
+      time.sleep(0.1)
+    
+    
+    pafn.frame_draw_cross(screen, initial_point, pafn.colors["yellow"])
+    pafn.frame_draw_cross(screen, goal_pt, pafn.colors["yellow"])
+    pygame.display.update()
+      
+
+
+
+    time.sleep(5)
+    sys.exit()
+
+
+def main():
+    # pl = generate_points(input_sequence=gen_van_der_corput())
+    pl = get_rand_sequence(600)
+    # print(pl)
+
+    # sys.exit()
+    pygame.init()
+    screen = pafn.create_display(1000, 1000)
+    pafn.clear_frame(screen)
+    pygame.display.update()
+    # sequence_test(screen, pl)
+    sequence_with_goal_test(screen, pl)
+    # triangle_test(screen)
+
+
+if __name__ == "__main__":
+    main()
